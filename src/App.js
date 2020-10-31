1715486a323a117fe7c8a20351199dabb3e158d7
Vincent Leclerc <v.leclerc90@gmail.com>
2020-10-31 15:57:48 +0100

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, World !
+          I love Docker!
         </p>
         <a
           className="App-link"
